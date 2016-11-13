e84ce96e5b66b7d6cd16521c573d2e51581ad91c
Unknown <st0ck53y@gmail.com>
2016-11-13 16:00:31 +0000

    minor cleanup

--- app/src/test/java/com/st0ck53y/testsudokuapplication/SolverTest.java
+++ app/src/test/java/com/st0ck53y/testsudokuapplication/SolverTest.java
@@ -20,7 +20,7 @@ public class SolverTest {
                              {0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0}};
         Solver s = new Solver(startGrid);
         s.solvePuzzle();
-        int[][] actualSol = s.getSolution(0);
+        int[][] actualSol = s.getSolution();
         for (int i = 0; i < 9; i++) {
             assertArrayEquals(expectedSol[i], actualSol[i]);
         }
@@ -39,7 +39,7 @@ public class SolverTest {
                              {9,6,1,2,8,7,3,4,5},{5,3,7,4,1,9,2,8,6},{2,8,4,6,3,5,1,7,9}};
         Solver s = new Solver(startGrid);
         s.solvePuzzle();
-        int[][] actualSol = s.getSolution(0);
+        int[][] actualSol = s.getSolution();
         for (int i = 0; i < 9; i++) {
             assertArrayEquals(expectedSol[i], actualSol[i]);
         }
