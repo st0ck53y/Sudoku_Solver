26675dee4959e4d59553e2511a44f58899128b2b
Unknown <st0ck53y@gmail.com>
2016-11-23 14:17:11 +0000

    Added gradient/direction calcs to NDK
    Speed up from 13-14 sec to 1.5-2 sec... very nice, main bottleneck now significantly reduced

--- app/src/main/cpp/ImageProcessing.cpp
+++ app/src/main/cpp/ImageProcessing.cpp
@@ -25,13 +25,17 @@ Java_com_st0ck53y_testsudokuapplication_classes_CameraView_nativeCanny(
     int* temp_buff2 = (int*)malloc((width*height)*sizeof(int));
 
 //    gaussianBlur(image_buffer, w, h, temp_buff);
-      gaussianBlur(image_buffer, w, h, output_buff);
-//    int *gradient = new int[width*height];
-//    int *direction = new int[width*height];
+    gaussianBlur(image_buffer, w, h, temp_buff);
+    int *gradient = (int*) malloc((width*height)*sizeof(int));
+    int *direction = (int*)malloc((width*height)*sizeof(int));
+
+    computeGradientAngles(temp_buff, w, h, output_buffer, direction);
 
     free(image_buffer);
     free(temp_buff);
     free(temp_buff2);
+    free(gradient);
+    free(direction);
 }
 
 void gaussianBlur(int* imgIn, int w, int h, int* output) {
@@ -70,3 +74,27 @@ void gaussianBlur(int* imgIn, int w, int h, int* output) {
     free(imgHor);
 }
 
+void computeGradientAngles(int* imgIn, int w, int h, int* gradient, int* direction) {
+    for (int y = 0; y < h - 1; y++) {
+        int yOffs = y * w;
+        int yPos  = yOffs + w;
+        for (int x = 0; x < w - 1; x++) {
+            double xd = computeXDerivative(imgIn[yOffs + x], imgIn[yOffs + x + 1], imgIn[yPos + x], imgIn[yPos + x + 1]);
+            double yd = computeYDerivative(imgIn[yOffs + x], imgIn[yOffs + x + 1], imgIn[yPos + x], imgIn[yPos + x + 1]);
+            gradient[yOffs+x] = (int) round(sqrt((xd*xd)+(yd*yd)));
+            direction[yOffs+x] =(int) round(toDegrees(atan(yd/xd)));
+        }
+    }
+}
+
+double computeXDerivative(int a, int b, int c, int d) {
+    return ((b - a) + (d - c))/2;
+}
+
+double computeYDerivative(int a, int b, int c, int d) {
+    return ((c - a) + (d - b))/2;
+}
+
+double toDegrees(double rad) {
+    return (rad * 180) / M_PI;
+}
--- app/src/main/cpp/ImageProcessing.h
+++ app/src/main/cpp/ImageProcessing.h
@@ -5,6 +5,7 @@
 #include <jni.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
 
 #ifndef SUDOKU_SOLVER_IMAGEPROCESSING_H
 #define SUDOKU_SOLVER_IMAGEPROCESSING_H
@@ -12,3 +13,7 @@
 #endif //SUDOKU_SOLVER_IMAGEPROCESSING_H
 
 void gaussianBlur(int* imgIn, int w, int h, int* output);
+void computeGradientAngles(int* imgIn, int w, int h, int* gradient, int* direction);
+double computeXDerivative(int a, int b, int c, int d);
+double computeYDerivative(int a, int b, int c, int d);
+double toDegrees(double rad);
